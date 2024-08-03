b04a6305492f91f6ab12129a23458ebddece4b5f
my-name <my-email>
2024-08-03 15:49:38 +0530

    this is just adding comment

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/nav'
 import './index.css'
-
+// hello this is made by rps
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
